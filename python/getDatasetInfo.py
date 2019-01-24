c9abd934d2646e7be6abc336e005f63399430a8d
Eric Schanet <eric.schanet@cern.ch>
2019-01-24 11:20:12 +0100

    Checking dataset availability

--- python/getDatasetInfo.py
+++ python/getDatasetInfo.py
@@ -2,11 +2,16 @@
 
 import os
 import re
+import pprint
 import pyAMI.client
+import pyAMI.atlas.api as AtlasAPI
 import argparse
 
+import json, ast
+
 parser = argparse.ArgumentParser(description='Helper to get information about a list of datasets')
-parser.add_argument('datasets', type=str, help='file containing list of datasets')
+# parser.add_argument('datasets', type=str, help='file containing list of datasets')
+parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
 
 args = parser.parse_args()
 
@@ -14,14 +19,20 @@ client = pyAMI.client.Client(
   'atlas'
  )
 
-with open (args.datasets) as datafile:
-    print "Did not find the following datasets:"
-    for dataset in datafile:
+print "INFO  -  Checking AMI status of datasets"
+
+for f in args.file:
+    print "INFO  -  Checking %s" %f
+
+    for dataset in f:
         dataset = dataset.rstrip()
         if dataset[:1] == '#':
             continue
         if dataset[:2] == 'mc':
-            ds = "-logicalDatasetName=%s" % dataset
-            result = client.execute(['GetDatasetInfo', ds])
-            if re.search(r'error', result):
-                print dataset
+            try:
+                result = AtlasAPI.get_dataset_info(client, dataset = dataset)
+                result = ast.literal_eval(json.dumps(result))
+                if not result[0]['prodsysStatus'] =='ALL EVENTS AVAILABLE':
+                    print("WARNING  -  Not ready yet - %s" %dataset)
+            except:
+                print("ERROR  -  Not in AMI - %s" %dataset)
