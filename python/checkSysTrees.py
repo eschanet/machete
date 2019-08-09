b5716e47fcb35ccb0ac1d2d67ebef92693dc1b26
Eric Schanet <eric.schanet@cern.ch>
2019-08-09 15:45:04 +0200

    Differentiate between signal and bkg trees

--- python/checkSysTrees.py
+++ python/checkSysTrees.py
@@ -17,7 +17,7 @@ logger = logger.getChild("mephisto")
 
 parser = argparse.ArgumentParser(description='This script checks if all processes in a root file have the same trees. You do not want to miss one of the input trees, e.g. one of the 10 bazillions of systematic trees.')
 parser.add_argument('file', help='ROOT file that you want to check')
-parser.add_argument('-p', '--processes', nargs='+', help='List of processes', default=["ttbar", "wjets", "zjets", "vh", "tth", "diboson", "multiboson", "singletop", "ttv"])
+parser.add_argument('-s', '--signal', action="store_true",help="Use signal tree naming convention (default is background trees)")
 
 args = parser.parse_args()
 
@@ -27,12 +27,18 @@ output, error = process.communicate()
 
 trees = output.split()
 
-
-raw_names = ["_".join(fullname.split("_")[:5]) for fullname in trees]
+if args.signal:
+    naming = 5
+else:
+    naming = 1
+raw_names = ["_".join(fullname.split("_")[:naming]) + "_" for fullname in trees]
 
 unique_names = list(set(raw_names))
 
 split_trees = [[t for t in trees if b in t] for b in unique_names]
 
+length = len(split_trees[0])
 for (l,bkg) in zip(split_trees,unique_names):
     logger.info("For process {}: {} trees".format(bkg, len(l)))
+    if not len(l) == length:
+         logger.error("Not the right length!")
