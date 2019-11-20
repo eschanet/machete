81c2b74a7e4996ab95859aa9ba2cb96f8d45e776
Eric Schanet <eric.schanet@cern.ch>
2019-11-20 16:06:53 +0100

    Adding norm factors for strong1l

---
+++ python/machete/nf.py
@@ -0,0 +1,38 @@
+import collections
+
+normFactorsDict = collections.OrderedDict()
+
+################################################################################################
+#
+#                       Strong1L
+#
+################################################################################################
+
+normFactorsDict["mu_Top_2J_bin1"] = 1.0096e+00
+normFactorsDict["mu_Top_2J_bin2"] = 9.3570e-01
+normFactorsDict["mu_Top_2J_bin3"] = 8.1392e-01
+normFactorsDict["mu_Top_2J_bin4"] = 7.2449e-01
+normFactorsDict["mu_Top_4Jhighx_bin1"] = 8.8389e-01
+normFactorsDict["mu_Top_4Jhighx_bin2"] = 7.5513e-01
+normFactorsDict["mu_Top_4Jhighx_bin3"] = 5.6046e-01
+normFactorsDict["mu_Top_4Jlowx_bin1"] = 7.8994e-01
+normFactorsDict["mu_Top_4Jlowx_bin2"] = 8.8801e-01
+normFactorsDict["mu_Top_4Jlowx_bin3"] = 5.2861e-01
+normFactorsDict["mu_Top_6J_bin1"] = 9.5101e-01
+normFactorsDict["mu_Top_6J_bin2"] = 8.1936e-01
+normFactorsDict["mu_Top_6J_bin3"] = 6.6983e-01
+normFactorsDict["mu_Top_6J_bin4"] = 6.6138e-01
+normFactorsDict["mu_W_2J_bin1"] = 1.0025e+00
+normFactorsDict["mu_W_2J_bin2"] = 7.9229e-01
+normFactorsDict["mu_W_2J_bin3"] = 6.9787e-01
+normFactorsDict["mu_W_2J_bin4"] = 5.8754e-01
+normFactorsDict["mu_W_4Jhighx_bin1"] = 7.9311e-01
+normFactorsDict["mu_W_4Jhighx_bin2"] = 7.1916e-01
+normFactorsDict["mu_W_4Jhighx_bin3"] = 7.6109e-01
+normFactorsDict["mu_W_4Jlowx_bin1"] = 8.8610e-01
+normFactorsDict["mu_W_4Jlowx_bin2"] = 8.3100e-01
+normFactorsDict["mu_W_4Jlowx_bin3"] = 7.4580e-01
+normFactorsDict["mu_W_6J_bin1"] = 6.9563e-01
+normFactorsDict["mu_W_6J_bin2"] = 5.6539e-01
+normFactorsDict["mu_W_6J_bin3"] = 5.1362e-01
+normFactorsDict["mu_W_6J_bin4"] = 2.9905e-01
